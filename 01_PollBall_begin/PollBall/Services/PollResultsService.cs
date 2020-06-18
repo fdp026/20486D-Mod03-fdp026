55b5354fcf9e6b21fdae00f001071a2e16f8e852
Don Phillips <phillips270335@bellsouth.net>
2020-06-18 16:09:51 -0400

    Completed Module 03 Lab Exercises 1, 2, 3

---
+++ 01_PollBall_begin/PollBall/Services/PollResultsService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PollBall.Services
+{
+    public class PollResultsService : IPollResultsService
+    {
+        private Dictionary<SelectedGame, int> _selectedVotes;
+
+        public PollResultsService()
+        {
+            _selectedVotes = new Dictionary<SelectedGame, int>();
+        }
+
+        public void AddVote(SelectedGame game)
+        {
+            if ( _selectedVotes.ContainsKey(game))
+            {
+                _selectedVotes[game]++;
+            }
+            else
+            {
+                _selectedVotes.Add(game, 1);
+            }
+        }
+
+        public SortedDictionary<SelectedGame, int> GetVoteResult()
+        {
+            return new SortedDictionary<SelectedGame, int>(_selectedVotes);
+        }
+    }
+}
